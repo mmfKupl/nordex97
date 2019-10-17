b7cc57c2405818bd148a58edeb697b2e7604020c
Ivan Kuplevich <ikuplevich97@gmail.com>
2019-10-17 21:09:51 +0300

    add fonts

--- proraw-frontend/src/app/requisites/requisites.component.ts
+++ proraw-frontend/src/app/requisites/requisites.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-class Requisite{
+class Requisite {
   name: string;
   info: string;
 }
@@ -13,15 +13,20 @@ class Requisite{
 export class RequisitesComponent implements OnInit {
   constructor() {}
 
-  requisites: Requisite[] =
-  [
-      {name: "ОГРН", info: "1166733073418"},
-      {name: "ОКПО", info: "05504099"},
-      {name: "ИНН/КПП", info: "6732135720 / 673201001"},
-      {name: "Юридический адрес", info: "РФ, 214032, г.Смоленск, ул.Маршала Еременко, д.8Б, офис Р2"},
-      {name: "Фактический (почтовый) адрес", info: "РФ, 109387, г.Москва, ул.Люблинская, д.42, офис 326-1"},
-      {name: "Контактный номер", info: "8-499-967-86-99"}
+  requisites: Requisite[] = [
+    { name: 'ОГРН', info: '1166733073418' },
+    { name: 'ОКПО', info: '05504099' },
+    { name: 'ИНН/КПП', info: '6732135720 / 673201001' },
+    {
+      name: 'Юридический адрес',
+      info: 'РФ, 214032, г.Смоленск, ул.Маршала Еременко, д.8Б, офис Р2'
+    },
+    {
+      name: 'Фактический (почтовый) адрес',
+      info: 'РФ, 109387, г.Москва, ул.Люблинская, д.42, офис 326-1'
+    },
+    { name: 'Контактный номер', info: '8-499-967-86-99' }
   ];
-  
+
   ngOnInit() {}
 }
