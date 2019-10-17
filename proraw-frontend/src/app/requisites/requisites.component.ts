3f4482c2762c922bf2872ddd2dd5ca0f96760afb
fulelgupport <chirk0ff.poker@gmail.com>
2019-10-17 19:30:55 +0300

    add header + requisites

--- proraw-frontend/src/app/requisites/requisites.component.ts
+++ proraw-frontend/src/app/requisites/requisites.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+class Requisite{
+  name: string;
+  info: string;
+}
+
 @Component({
   selector: 'app-requisites',
   templateUrl: './requisites.component.html',
@@ -8,5 +13,15 @@ import { Component, OnInit } from '@angular/core';
 export class RequisitesComponent implements OnInit {
   constructor() {}
 
+  requisites: Requisite[] =
+  [
+      {name: "ОГРН", info: "1166733073418"},
+      {name: "ОКПО", info: "05504099"},
+      {name: "ИНН/КПП", info: "6732135720 / 673201001"},
+      {name: "Юридический адрес", info: "РФ, 214032, г.Смоленск, ул.Маршала Еременко, д.8Б, офис Р2"},
+      {name: "Фактический (почтовый) адрес", info: "РФ, 109387, г.Москва, ул.Люблинская, д.42, офис 326-1"},
+      {name: "Контактный номер", info: "8-499-967-86-99"}
+  ];
+  
   ngOnInit() {}
 }
